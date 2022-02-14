c635e5f852e7f7e358dd0dd2c9fcce9f0fc669ba
Egor <egor.vas2004@gmail.com>
2022-02-14 16:57:10 +0200

    fixed some errors

--- Lab1Py/Lab1Py.py
+++ Lab1Py/Lab1Py.py
@@ -19,42 +19,29 @@ def read_text_from_file(path): #зчитування тексту з файлу там перетворення рядкі
             line=text_from_file.readline()
             if not line:
                 break
-            text.append(line.strip(',.\n ').split())
+            text.append(line.replace(',', ' ').replace('.', ' ').strip('\n ').split())
     return text
 
-def N_times(text, n): #повертає новий список з словами, що повторюються більше n разів
+def N_times(text, n): #повертає список з словами, що повторюються більше n разів
     repeated_words=[] #список що зберігає слова, які повторюються
-    new_text=copy.deepcopy(text) #копія з якої видалятимемо слова, що не повторюються
     for line in text:
         for word in line:
-            if word not in repeated_words:
+            if word.lower() not in repeated_words:
                 num_of_repeats=0
-                for line_c in text:
-                    for word_c in line_c:
+                for line_cur in text:
+                    for word_c in line_cur:
                         if word_c.lower()==word.lower():
                             num_of_repeats+=1                  
                 if num_of_repeats>n:
-                    repeated_words.append(word)
-    for i in range(len(text)):
-        for j in range(len(text[i])):
-            if text[i][j] not in repeated_words:
-                new_text[i].remove(text[i][j]) #видалення слів з копії списку
-    return new_text   
+                    repeated_words.append(word.lower())
+    return repeated_words   
 
 def sort(text): #сортування списку за довжиною
-    for line in text:
-        for i in range(len(line)-1):
-            for j in range(len(line)-i-1):
-                if len(line[j])>len(line[j+1]):
-                    line[j+1],line[j]=line[j],line[j+1]
-    return text
-
-def list_to_string(text): #перетворення списку у рядки, зі збереженням Enter'рів
-    str_text=''
-    for line in text:
-        if line:
-            str_text=str_text+" ".join(line)+"\n"
-    return str_text
+        for i in range(len(text)-1):
+            for j in range(len(text)-i-1):
+                if len(text[j])>len(text[j+1]):
+                    text[j+1],text[j]=text[j],text[j+1]
+        return " ".join(text)
 
 path1='oldFile.txt' #початковий файл
 path2='newFile.txt' #кінцевий файл
@@ -62,7 +49,7 @@ text=input_str("Write your text in the console below \nPress Ctrl+x and then \"E
 n=int(input("Write number of repetitions: "))
 write_text_to_file(path1, text)
 text=read_text_from_file(path1)
-text=list_to_string(sort(N_times(text, n)))
+text=(sort(N_times(text, n)))
 print(text)
 write_text_to_file(path2,text) 
 
