df11018a8884342017ec14ce24b035084d97794b
Egor <egor.vas2004@gmail.com>
2022-04-09 00:46:55 +0300

    changed input function; added new opportunity to input date

--- Lab4C++/Lab4C++.cpp
+++ Lab4C++/Lab4C++.cpp
@@ -6,40 +6,159 @@
 
 using namespace std;
 
-class date 
+class date
 {
 private:
     int day, month, year;
 public:
-    date();
-    date(int day, int month, int year);
+    date(int day = 12, int month = 12, int year = 2012);
     date(string date);
+    date(int day, string month, int year);
+    int get_day();
+    int get_month();
+    int get_year();
+    void print_info();
+    string get_season();
 };
+date input();
 
 int main()
 {
-    date p("12/12/1222");
+    date D1 = input();
+    D1.print_info();
+    date D2 = input();
+    D2.print_info();
+    date D3 = input();
+    D3.print_info();
 }
 
-date::date()
-{
-    cout << "default constructor" << endl;
-    this->day = 12;
-    this->month = 12;
-    this->year = 2012;
-}
 date::date(int day, int month, int year)
 {
     this->day = day;
     this->month = month;
     this->year = year;
 }
+date::date(int day, string month, int year)
+{
+    this->day = day;
+    this->year = year;
+    for (auto& c : month)
+    {
+        c = tolower(c);
+    }
+    if (month == "january")
+        this->month = 1;
+    else if (month == "february")
+        this->month = 2;
+    else if (month == "march")
+        this->month = 3;
+    else if (month == "april")
+        this->month = 4;
+    else if (month == "may")
+        this->month = 5;
+    else if (month == "june")
+        this->month = 6;
+    else if (month == "july")
+        this->month = 7;
+    else if (month == "august")
+        this->month = 8;
+    else if (month == "september")
+        this->month = 9;
+    else if (month == "october")
+        this->month = 10;
+    else if (month == "november")
+        this->month = 11;
+    else if (month == "december")
+        this->month = 12;
+    else 
+        this->month = 12;
+}
 date::date(string date)
 {
-    int pos_first = date.find('/');
-    int pos_last = date.rfind('/');
-    day = stoi(date.substr(0, pos_first));
-    month = stoi(date.substr(pos_first+1, pos_last-pos_first));
-    year = stoi(date.substr(pos_last+1, date.length()- pos_last));
+    if (date.find('/') != -1)
+    {
+        int pos_first = date.find('/');
+        int pos_last = date.rfind('/');
+        day = stoi(date.substr(0, pos_first));
+        month = stoi(date.substr(pos_first + 1, pos_last - pos_first));
+        year = stoi(date.substr(pos_last + 1, date.length() - pos_last));
+    }
+    else
+    {
+        int pos_first = date.find('.');
+        int pos_last = date.rfind('.');
+        day = stoi(date.substr(0, pos_first));
+        month = stoi(date.substr(pos_first + 1, pos_last - pos_first));
+        year = stoi(date.substr(pos_last + 1, date.length() - pos_last));
+    }
 }
-//verify
+int date::get_day()
+{
+    return day;
+}
+int date::get_month()
+{
+    return month;
+}
+int date::get_year()
+{
+    return year;
+}
+void date::print_info()
+{
+    printf("\n%02i.%02i.%04i\n", get_day(), get_month(), get_year());
+}
+string date::get_season()
+{
+    return "not done";
+}
+
+
+date input()
+{
+    int mode;
+    cout << "\nYou can input date in format" << endl;
+    cout << "1:   day=xx, month=xx, year=xxxx" << endl;
+    cout << "2:   xx/xx/xxxx (xx.xx.xxxx)" << endl;
+    cout << "3:   day=xx, month='*******', year=xxxx" << endl;
+    cin >> mode;
+    int day, month, year;
+    string month_str, date_str;
+    date D1;
+    switch (mode)
+    {
+    case 1:
+    {
+        cout << "Input day: ";
+        cin >> day;
+        cout << "Input month: ";
+        cin >> month;
+        cout << "Input year: ";
+        cin >> year;
+        date A(day, month, year);
+        return A;
+    }
+    case 2:
+    {
+        cout << "Input date: ";
+        cin >> date_str;
+        date A(date_str);
+        return A;
+    }
+    case 3:
+    {
+        cout << "Input day: ";
+        cin >> day;
+        cout << "Input month (word): ";
+        cin >> month_str;
+        cout << "Input year: ";
+        cin >> year;
+        date A(day, month_str, year);
+        return A;
+    }
+    default:
+        cout << "Incorrect mode" << endl;
+        date A;
+        return A;
+    }
+}
