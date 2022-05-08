200f0edfe7c31ab2b623a4487dfbdd84284fe2e2
Egor <egor.vas2004@gmail.com>
2022-05-08 19:00:16 +0300

    changed get_length and scalar methods

--- Lab5Py/TVector2.py
+++ Lab5Py/TVector2.py
@@ -1,16 +1,17 @@
 from TVector import TVector
 from math import sqrt
+from math import pow
 
 class TVector2(TVector):
     def __init__(self, x=0, y=0):
         self.__x=x 
         self.__y=y
     def get_length(self):
-        return sqrt(self.__x*self.__x+self.__y*self.__y)
+        return sqrt(pow(self.__x,2)+pow(self.__y,2))
     def get_coord(self, coord):
             return self.__x if coord=='x' else self.__y
     def scalar(self, other):
-        return self.__x*other.get_coord('x')+self.y*other.get_coord('y')
+        return self.__x*other.get_coord('x')+self.__y*other.get_coord('y')
 
     
    
